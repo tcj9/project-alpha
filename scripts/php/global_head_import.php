d9c7530d185d3933aa0b9ae842b26dd3dc6cd2da
Travaro Jones <travarojones9@gmail.com>
2024-07-20 12:29:09 -0400

    fixed global_head_import issue

--- scripts/php/global_head_import.php
+++ scripts/php/global_head_import.php
@@ -1,6 +1,8 @@
 <!-- global_head_imports -->
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="libs/bulma.min.css">
-<link rel="stylesheet" href="libs/icons.css">
-<link rel="stylesheet" href="styles/global.css">
+<?php
+echo '<meta charset="UTF-8">';
+echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+echo '<link rel="stylesheet" href="libs/bulma.min.css">';
+echo '<link rel="stylesheet" href="libs/icons.css">';
+echo '<link rel="stylesheet" href="styles/global.css">';
+?>
